d43ec03ef9553e4158966195c9ecdb20b421d29a
Chrismis79 <chrismis79@gmail.com>
2019-12-13 11:37:50 -0600

    migrations done

---
+++ data/migrations/20191213110407_projects_table.js
@@ -0,0 +1,57 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', tbl => {
+        tbl.increments();
+        tbl.text('proj_name', 128)
+            .unique()
+            .notNullable();
+        tbl.text('proj_description', 300);
+        tbl.boolean('completed')
+            defaultTo(false);
+    })
+    .createTable('resources', tbl => {
+        tbl.increments();
+        tbl.text('resource_name')
+            .unique()
+            .notNullable();
+        tbl.text('description', 300)
+    })
+    .createTable('project_resources', tbl => {
+        tbl.primary(['project_id', 'resource_id']);
+        tbl.integer('project_id')
+          .unsigned()
+          .notNullable()
+          .reference('id')
+          .inTable('projects');
+        tbl.integer('resource_id')
+          .unsigned()
+          .notNullable()
+          .reference('id')
+          .inTable('resources');
+    })
+    .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.text('task_description')
+          .notNullable()
+          .unique();
+        tbl.text('notes', 300);
+        tbl.boolean('completed')
+            .defaultTo(false);
+        tbl.integer('project_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('projects')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE');
+      });
+  };
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects');
+};
--- data/projects/project-router.js
+++ data/projects/project-router.js
@@ -4,4 +4,11 @@ const Projects = require('./project-model.js');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+    console.log("User router");
+    next();
+  })
+
+
+
 module.export = router;
